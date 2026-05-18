6750a891feecccc92dcf22b71b6d8b5b0d476026
Hectornado123 <Hecalbgon1@gmail.com>
2026-05-18 17:14:59 +0200

    aaaaaaa

--- Assets/Scripts/Rata.cs
+++ Assets/Scripts/Rata.cs
@@ -7,17 +7,24 @@ public class PatrullaAB : MonoBehaviour
     public Transform puntoA;
     public Transform puntoB;
     public float velocidadGiro = 180f;
+    public float tiempoIdle = 2f; // duración del idle
 
     private NavMeshAgent agent;
     private Transform destino;
     private bool girando = false;
+    private Animator anim;
 
     void Start()
     {
-       
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        agent.updateRotation = false;
+
         destino = puntoA;
         agent.SetDestination(destino.position);
+
+        anim.SetBool("Caminar", true);
     }
 
     void Update()
@@ -33,13 +40,20 @@ public class PatrullaAB : MonoBehaviour
         girando = true;
         agent.isStopped = true;
 
+        // Idle
+        anim.SetBool("Caminar", false);
+
+        // Espera a que termine la animación idle
+        yield return new WaitForSeconds(tiempoIdle);
+
         destino = (destino == puntoA) ? puntoB : puntoA;
 
         Vector3 direccion = destino.position - transform.position;
         direccion.y = 0;
 
-        Quaternion rotacionObjetivo = Quaternion.LookRotation(direccion);
+        Quaternion rotacionObjetivo = Quaternion.LookRotation(direccion) * Quaternion.Euler(0, 180, 0);
 
+        // Giro después del idle
         while (Quaternion.Angle(transform.rotation, rotacionObjetivo) > 1f)
         {
             transform.rotation = Quaternion.RotateTowards(
@@ -50,6 +64,9 @@ public class PatrullaAB : MonoBehaviour
 
             yield return null;
         }
+
+        // Vuelve a caminar
+        anim.SetBool("Caminar", true);
 
         agent.SetDestination(destino.position);
         agent.isStopped = false;
