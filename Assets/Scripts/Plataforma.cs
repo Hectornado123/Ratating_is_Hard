113f1bdbf0304f79e471c6b19276fd2c5563985e
Hectornado123 <Hecalbgon1@gmail.com>
2026-05-20 15:26:45 +0200

    a

--- Assets/Scripts/Plataforma.cs
+++ Assets/Scripts/Plataforma.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Plataforma : MonoBehaviour
+public class Plataformacxz : MonoBehaviour
 {
     public Transform puntoA;
     public Transform puntoB;
